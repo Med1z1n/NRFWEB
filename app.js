26b7a72e8de8444df224804f14bd9f6be5b4ba19
Med1z1n <99459061+Med1z1n@users.noreply.github.com>
2025-09-23 11:33:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function handleNotification(event) {
     const value = event.target.value;
     const rawMessage = new TextDecoder().decode(value);
     console.log(rawMessage);
-    If the message starts with 1: or 2:, split into code + message
+    //If the message starts with 1: or 2:, split into code + message
     if (rawMessage.startsWith("1:") || rawMessage.startsWith("2:")) {
         const [code, ...rest] = rawMessage.split(":");
         const message = rest.join(":"); // handles extra ":" in text
